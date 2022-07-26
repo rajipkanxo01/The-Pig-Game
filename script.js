0668ffc634a23a6c8d328fe101f639ffed7481e2
rajipkanxo01 <82520479+rajipkanxo01@users.noreply.github.com>
2022-07-26 23:52:50 +0200

    function: roll dice & change to change player

--- script.js
+++ script.js
@@ -1,14 +1,63 @@
-const player1 = document.querySelector('.player-1').classList;
-const player2 = document.querySelector('.player-2').classList;
+//selecting elements
+const score1El = document.getElementById('score-1');
+const score2El = document.getElementById('score-2');
+const diceEl = document.querySelector('.dice');
+const holdButton = document.querySelector('.btn-hold');
+const rollButton = document.querySelector('.btn-roll');
+const newGameButton = document.querySelector('.btn-new');
+const currentScore1El = document.getElementById('current-1');
+const currentScore2El = document.getElementById('current-2');
+const player1El = document.querySelector('.player-1');
+const player2El = document.querySelector('.player-2');
+
+
+//starting conditions
+score1El.textContent = 0;
+score2El.textContent = 0;
+diceEl.classList.add('hidden');
+
+
+const score = [0,0];
+// 1 for player 1 and 2 for player 2
+let activePlayer = 1;
+let currentScore = 0;
 
 
 
-// change between player 1 and player 2
-function changePlayer() {
-    player1.contains('player-active') ? player1.remove('player-active')
-        : player1.add('player-active');
-    player2.contains('player-active') ? player2.remove('player-active')
-        : player2.add('player-active');
+
+
+// rolling dice
+function rollDice() {
+    // generating random number to be dice number
+    const diceNumber = Math.ceil(Math.random() * 6);
+    diceEl.classList.remove('hidden');
+
+    // display random dice
+    diceEl.src = `images/dice-${diceNumber}.png`;
+
+    // change player if roll is 1
+    if (diceNumber !== 1) {
+        currentScore += diceNumber;
+        document.getElementById(`current-${activePlayer}`).textContent = currentScore;
+    } else {
+        changePlayer();
+    }
 }
 
-document.querySelector('.btn--hold').addEventListener('click', changePlayer);
+rollButton.addEventListener('click', rollDice);
+
+
+// change between player 1 and player 2 functionality
+function changePlayer() {
+    document.getElementById(`current-${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    // active player value changes to 2 if current value is 1 and vice versa
+    activePlayer = activePlayer === 1 ? 2 : 1;
+    player1El.classList.toggle('player-active');
+    player2El.classList.toggle('player-active');
+}
+
+holdButton.addEventListener('click', changePlayer);
+
+
+// add scoring
